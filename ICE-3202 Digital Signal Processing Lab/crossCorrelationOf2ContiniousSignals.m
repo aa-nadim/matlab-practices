20c9a75b4ebbfdad38a279112875b9c325f113c5
aa-nadim <nadim.ice.nstu@gmail.com>
2021-04-26 23:39:41 +0600

    cross-correlation of two signals

---
+++ ICE-3202 Digital Signal Processing Lab/crossCorrelationOf2ContiniousSignals.m
@@ -0,0 +1,27 @@
+% Cross-correlation of two Continious Signals
+clc
+clear all
+t = 0:0.001:0.1;
+f = 50;     % frequency
+t1 = 2*pi*f*t; 	%We know that, sin(wt); w=2*pi*T;
+
+x1 = sin(t1);
+subplot(3,1,1);
+plot(t,x1);
+title('Sine Wave')
+xlabel('Time Period');
+ylabel('Amplitude');
+
+x2 = cos(t1);
+subplot(3,1,2);
+plot(t,x2);
+title('Cosine Wave')
+xlabel('Time Period');
+ylabel('Amplitude');
+
+y = xcorr(x1,x2);
+subplot(3,1,3);
+plot(y);
+title('Cross-correlation of two signals');
+xlabel('Time Period');
+ylabel('Amplitude');
---
+++ ICE-3202 Digital Signal Processing Lab/crossCorrelationOf2DiscreteSignals.m
@@ -0,0 +1,26 @@
+% Cross-correlation of two Discrete Signals
+clc
+clear all
+a = [1 1 2 2];
+
+subplot(3,1,1);
+stem(a);
+title('1st Signal');
+xlabel('Time Period');
+ylabel('Amplitude');
+
+b = [1 3 1];
+
+subplot(3,1,2);
+stem(b);
+title('2nd Signal');
+xlabel('Time Period');
+ylabel('Amplitude');
+
+y = xcorr(a,b);
+
+subplot(3,1,3);
+stem(y);
+title('Cross-correlation of two signals');
+xlabel('Time Period');
+ylabel('Amplitude');
