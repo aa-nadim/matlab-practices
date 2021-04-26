a893a385e7d68eab6dc8c3b75844e30a4076b609
aa-nadim <nadim.ice.nstu@gmail.com>
2021-04-26 23:06:29 +0600

    Add 2 discrete and continuous signals

--- ICE-3202 Digital Signal Processing Lab/add2ContiniousSignals.m
+++ ICE-3202 Digital Signal Processing Lab/add2ContiniousSignals.m
@@ -10,13 +10,19 @@ x1 = sin(t1);
 subplot(3,1,1);
 plot(t,x1);
 title('Sine Wave')
+xlabel('Time Period');
+ylabel('Amplitude');
 
 x2 = cos(t1);
 subplot(3,1,2);
 plot(t,x2);
 title('Cosine Wave')
+xlabel('Time Period');
+ylabel('Amplitude');
 
 y = x1+x2;
 subplot(3,1,3);
 plot(t,y);
 title('Addition of two signals')
+xlabel('Time Period');
+ylabel('Amplitude');
--- ICE-3202 Digital Signal Processing Lab/add2DiscreteSignals.m
+++ ICE-3202 Digital Signal Processing Lab/add2DiscreteSignals.m
@@ -7,13 +7,19 @@ a = [1 2 -3 4 1 2 3];
 subplot(3,1,1)
 stem(t,a)
 title('input a(n)')
+xlabel('Time Period');
+ylabel('Amplitude');
 
 b = [1 -1 1 0 1 1 -1];
 subplot(3,1,2)
 stem(t,b)
 title('input b(n)')
+xlabel('Time Period');
+ylabel('Amplitude');
 
 y = a+b;
 subplot(3,1,3)
 stem(t,y)
 title('input y(n)')
+xlabel('Time Period');
+ylabel('Amplitude');
